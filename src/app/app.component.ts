e36ac5dd7e735fc46b844bbf9c2c874d472f3647
Ashish Upadhyay <ashishupadhyay855@gmail.com>
2017-07-21 18:15:01 +0530

    add title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
-  title = 'app';
+  
+  title = 'Tour of Heroes';
+  hero = 'Windstorm';
 }
+
