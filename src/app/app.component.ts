f7353c136a29ce653de88d7024db2f17af3ca63d
Moin Shaikh <shaikhmoin250@gmail.com>
2017-07-21 16:37:08 +0530

    add a class Hero with id and name properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+export class hero{
+  id:number;
+  name:string;
+}
 export class AppComponent {
   title = 'Tour of Heroes';
   hero= 'Windstorm';
